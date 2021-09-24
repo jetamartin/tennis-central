a566147dc5c876c651be5280aba58841b65e7fc8
Paul Sherer <wolfymaster@hotmail.com>
2021-09-24 16:53:58 +0000

    LoginTest

--- source-code/frontend/src/components/Login.test.js
+++ source-code/frontend/src/components/Login.test.js
@@ -2,10 +2,11 @@ import React from "react";
 import { render, fireEvent, waitFor } from "@testing-library/react";
 import Login from "./Login";
 
-function loginUser() {
-  console.log('In loginUser')
-  return {
+import {Router} from 'react-router-dom'
+import {createMemoryHistory} from 'history'
 
+function loginUser() {
+  return {
     userinfo: {
       token: "ABC",
       userId: 3,
@@ -21,26 +22,33 @@ it("renders without crashing", function () {
 
 
 it("Allow the user to successfully login", async () => {
-  debugger;
+  // createa history object - otherwise useHistory() will throw an error
+  const history = createMemoryHistory()
+  // create a spy to spy on the 'push' method of our history object
+  const pushSpy = jest.spyOn(history, 'push')
+
+  // wrap our component in a Router which provides our history object to our Login component
+  // via the useHistory() hook
   const { queryByText, getByLabelText, getByText, debug } = render(
-    <Login loginUser={loginUser} />
+    <Router history={history}>
+      <Login loginUser={loginUser} />
+    </Router>
   );
+
   const userNameInput = getByLabelText("Username");
   const passwordInput = getByLabelText("Password");
   const submitBtn = queryByText("Submit");
-  console.log(submitBtn);
-  
-  await waitFor(() => {
-    fireEvent.change(userNameInput, { target: { value: "jMartin" } });
-    fireEvent.change(passwordInput, { target: { value: "1234" } });
-    fireEvent.click(submitBtn);
-  });
-  await waitFor(() => {
-    expect(queryByText("Logout jMartin")).toBeInTheDocument();
-    debug();
-  });
 
-  debugger;
+  // fire our events
+  fireEvent.change(userNameInput, { target: { value: "jMartin" } });
+  fireEvent.change(passwordInput, { target: { value: "1234" } });
+  fireEvent.click(submitBtn);
+
+  // wait for our functions to be called
+  await waitFor(() => {
+    // Expect that our spy was called with '/' 
+    expect(pushSpy).toHaveBeenCalledWith('/')
+  });
 });
 
 // it("works when you click on the right arrow", function() {
